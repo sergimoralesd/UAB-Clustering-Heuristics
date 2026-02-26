10dd4832e5da45daefef71e2a6df7f6072960b74
Sergi <1633942@uab.cat>
2026-02-26 12:08:30 +0100

    new tests

--- uab_heuristics/tests/test_heuristics.py
+++ uab_heuristics/tests/test_heuristics.py
@@ -7,7 +7,7 @@ from uab_heuristics.utils import get_collection_tx
 BASE_DIR = Path(__file__).resolve().parent
 
 
-def test_heuristic(txs, heuristic_cls, prev_tx=False, future_txs=None):
+def test_heuristic(txs, heuristic_cls, prev_tx=False, future_txs=False):
     print("#" * 50)
     if isinstance(heuristic_cls, type):
         heuristic = heuristic_cls()
@@ -15,18 +15,18 @@ def test_heuristic(txs, heuristic_cls, prev_tx=False, future_txs=None):
         heuristic = heuristic_cls
     print(f"Starting test with heuristic: {heuristic.name}")
 
-    for tx_id, tx_raw in txs:
+    for tx in txs:
+        tx_id = tx["tx_id"]
+        tx_raw = tx["tx_raw"]
         try:
             print(f"Applying heuristic to tx: \n{tx_id}")
-            tx = Tx.from_raw(tx_raw)
-            if prev_tx:
-                tx.import_previous_txs()
+            tx_object = Tx.from_raw(tx_raw)
             if future_txs:
-                tx.import_future_txs(future_txs)
+                tx_object.import_future_txs(tx["future_txs"])
                 
-            print(f"Input addrecess: \n{tx.input_addresses}")
-            print(f"Output addrecess: \n{tx.output_addresses}")
-            result_reused_addr_change = heuristic.apply(tx)
+            print(f"Input addrecess: \n{tx_object.input_addresses}")
+            print(f"Output addrecess: \n{tx_object.output_addresses}")
+            result_reused_addr_change = heuristic.apply(tx_object)
             print(f"Result from heurisitc: \n{result_reused_addr_change}")
         except Exception as e:
             print(e)
@@ -38,17 +38,19 @@ if __name__ == "__main__":
     filename = BASE_DIR / "data" / "tx_collection.json"
     txs = get_collection_tx(filename, ["change_address"])
 
-    test_heuristic(txs, ReusedAddressChange)
+    #test_heuristic(txs, ReusedAddressChange)
     
-    test_heuristic(txs, AddressTypeChange)
+    #test_heuristic(txs, AddressTypeChange)
 
-    for n in range(2, 8):
-        print(f"-- N:{n} --")
-        rounded_change = RoundedChange(n)
-        test_heuristic(txs, rounded_change)
+    #for n in range(2, 8):
+    #    print(f"-- N:{n} --")
+    #    rounded_change = RoundedChange(n)
+    #    test_heuristic(txs, rounded_change)
 
-    test_heuristic(txs, SmallerChange)
+    #test_heuristic(txs, SmallerChange)
 
-    test_heuristic(txs, OptimalChange, prev_tx=True)
+    #test_heuristic(txs, OptimalChange)
+
+    test_heuristic(txs, InputOrderChange, future_txs=True)
     
     
