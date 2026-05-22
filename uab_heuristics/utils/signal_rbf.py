22b5af436708f14712144ca62e701de8d0427455
Sergi <1633942@uab.cat>
2026-05-22 10:34:25 +0200

    minor correction

--- uab_heuristics/utils/signal_rbf.py
+++ uab_heuristics/utils/signal_rbf.py
@@ -1,5 +1,5 @@
 def signals_rbf(tx):
     for sequence_num in tx.inputs_sequence:
-        if sequence_num < 4294967295:
+        if sequence_num < 4294967294:
             return True
     return False
