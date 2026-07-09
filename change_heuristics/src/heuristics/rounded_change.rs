23202c01feb1814815d746625c4dd96d499de697
Sergi <1633942@uab.cat>
2026-07-09 13:11:00 +0200

    rounded change

---
+++ change_heuristics/src/heuristics/rounded_change.rs
@@ -0,0 +1,36 @@
+use crate::tx::Tx;
+use crate::types::{AppError, InputDataRequirements};
+
+use super::Heuristic; 
+
+pub struct RoundedChange {
+    precision_parameter: u8,
+}
+
+impl RoundedChange {
+    pub fn new(precision_parameter: u8) -> Self {
+        RoundedChange { precision_parameter }
+    }
+}
+
+impl Heuristic for RoundedChange {
+    fn name(&self) -> &str {
+        "RoundedChange"
+    }
+
+    fn input_data_requirements(&self) -> InputDataRequirements {
+        InputDataRequirements::None
+    }
+
+    fn apply(&self, tx: &Tx) -> Result<Vec<bool>, AppError> {
+        let _ = self.check_requirements(tx, false);
+
+        let precision: u64 = 10 ^ self.precision_parameter as u64;
+        let possible_change: Vec<bool> = tx.outputs_values()
+        .iter()
+        .map(|value| value % precision != 0)
+        .collect();
+
+        Ok(possible_change)
+    }
+}
