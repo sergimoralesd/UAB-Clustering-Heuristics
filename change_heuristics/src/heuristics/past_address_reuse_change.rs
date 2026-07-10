97de537e1186271fbf3a81a20187807dd1638636
Sergi <1633942@uab.cat>
2026-07-10 13:28:45 +0200

    adapted to new function api

--- change_heuristics/src/heuristics/past_address_reuse_change.rs
+++ change_heuristics/src/heuristics/past_address_reuse_change.rs
@@ -1,9 +1,8 @@
-use bitcoin::Address;
-
 use crate::tx::Tx;
-use crate::types::{TxError, ApiError, AppError, InputDataRequirements, JsonTx};
+use crate::types::{AppError, InputDataRequirements};
 
-use super::Heuristic; 
+use super::Heuristic;
+use super::api::get_txs_by_address; 
 
 pub struct PastReusedAddressChange;
 
@@ -24,7 +23,7 @@ impl Heuristic for PastReusedAddressChange {
 
         let mut possible_change: Vec<bool> = vec![false; outputs_addresses.len()];
         for (index, out_addr) in outputs_addresses.iter().enumerate() {
-            let all_txs_from_address: Vec<Tx> = get_txs_by_address(out_addr)?;
+            let all_txs_from_address: Vec<Tx> = get_txs_by_address(out_addr.to_string())?;
 
             let mut blocks_heights: Vec<usize> = all_txs_from_address
                 .iter()
@@ -48,50 +47,4 @@ impl Heuristic for PastReusedAddressChange {
         }
         Ok(possible_change)
     }
-}
-
-pub fn get_txs_by_address(addr: &Address) -> Result<Vec<Tx>, AppError> {
-    let url = format!(
-        "https://mempool.space/api/address/{}/txs/chain",
-        addr.to_string()
-    );
-
-    let response = reqwest::blocking::get(&url)
-        .map_err(|e| ApiError::UnableToFetch(e.to_string()))?;
-
-    let raw: serde_json::Value = response.json()
-        .map_err(|e| TxError::Decode(e.to_string()))?;
-
-    let result = raw.as_array()
-        .ok_or(TxError::Decode("expected array".to_string()))?
-        .iter()
-        .map(|item| {
-            // deserialize into JsonTx
-            let json_tx: JsonTx = serde_json::from_value(item.clone())
-                .map_err(|e| TxError::Decode(e.to_string()))?;
-
-            // extract block height from raw JSON separately
-            let block_height = item["status"]["block_height"]
-                .as_u64()
-                .map(|bh| bh as usize);
-
-            Ok((json_tx, block_height))
-        })
-        .collect::<Result<Vec<(JsonTx, Option<usize>)>, TxError>>()?;
-
-    let txs: Vec<Tx> = result
-    .iter()
-    .map(|(json_tx, block_height)| {
-            let mut tx = Tx::from_json(json_tx, bitcoin::Network::Bitcoin)
-            .map_err(|e| AppError::Tx(e))?;
-
-            if let Some(bh) = block_height {
-                tx.import_block_height(*bh);  
-            }
-        Ok(tx)
-    })
-    .collect::<Result<Vec<Tx>, AppError>>()?;
-
-    Ok(txs)
-    
 }
