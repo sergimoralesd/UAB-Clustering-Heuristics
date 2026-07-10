983940014e3efacc12433853b5c6e03b4ee24452
Sergi <1633942@uab.cat>
2026-07-10 07:56:42 +0200

    smaller output change

---
+++ change_heuristics/src/heuristics/smaller_output_change.rs
@@ -0,0 +1,29 @@
+use crate::tx::Tx;
+use crate::types::{AppError, InputDataRequirements};
+
+use super::Heuristic; 
+
+pub struct SmallerOuputChange;
+
+impl Heuristic for SmallerOuputChange {
+    fn name(&self) -> &str {
+        "SmallerOuputChange"
+    }
+
+    fn input_data_requirements(&self) -> InputDataRequirements {
+        InputDataRequirements::None
+    }
+
+    fn apply(&self, tx: &Tx) -> Result<Vec<bool>, AppError> {
+        let _ = self.check_requirements(tx, false);
+
+        let output_values = tx.outputs_values();
+        let min_value: &u64 = output_values.iter().min().unwrap();
+        let possible_change: Vec<bool> = tx.outputs_values()
+        .iter()
+        .map(|value| value == min_value)
+        .collect();
+
+        Ok(possible_change)
+    }
+}
