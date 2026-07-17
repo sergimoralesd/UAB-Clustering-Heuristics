ac576cce8a020825f39ecf1de81334c9e31f732b
Sergi <1633942@uab.cat>
2026-07-17 10:39:44 +0200

    unit tests added

--- change_heuristics/src/heuristics/fee_absolute_change.rs
+++ change_heuristics/src/heuristics/fee_absolute_change.rs
@@ -29,3 +29,49 @@ impl Heuristic for FeeAbsoluteChange {
         Ok(possible_change)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::heuristics::test_utils::build_tx;
+    use crate::types::TestError;
+    
+
+    #[test]
+    fn test_address_type_heuristic() -> Result<(),AppError> {
+        let tx_file = std::fs::File::open("tests/data/sampled_transactions.json")
+            .expect("JSON file was not formatted correctly");
+        let dict_txs: serde_json::Value = serde_json::from_reader(tx_file)
+            .expect("JSON was not well-formatted");
+
+        let txids = vec![
+            "40fe72e6cce5d7fa7c16a1714871215462956eef6138eeffa66338c9147c49f2", 
+            "eadaf24d88bb6d6333f2f6df7ba3508130dd501981633750497043fd4eb66d21", 
+            "f6ddb88c34223a4d9b358075e1201c0d9aa0b588bc35f87b390b8037564598cd"
+        ];
+
+        let expected_results = vec![
+            vec![false, true],
+            vec![true, true],
+            vec![true, false]
+        ];
+
+
+        for (txid, expected_result) in txids.iter().zip(expected_results) {
+            let tx_dict = match dict_txs.get(txid) {
+                None => return Err(AppError::Test(TestError::MissingTxid(format!("missing tx")))),
+                Some(tx) => tx
+            };
+
+            let tx = build_tx(tx_dict, &FeeAbsoluteChange.input_data_requirements(), false)?;
+
+            match FeeAbsoluteChange.apply(&tx) {
+                Err(err) => println!("Error: {}", err.to_string()),
+                Ok(res) => assert_eq!(res, expected_result)
+            };
+        }
+
+        Ok(())
+
+    }
+}
